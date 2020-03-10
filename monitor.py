e24fb78776f1c3a6a0b6747e8608880e85218762
Dimasyoga <dimasyogapra@gmail.com>
2020-03-11 01:06:03 +0700

    final

--- monitor.py
+++ monitor.py
@@ -9,26 +9,41 @@ ap.add_argument("-r", "--res", required=False,
     help="video output resolution")
 args = vars(ap.parse_args())
 
-if(args["res"] == 480):
+if(int(args["res"]) == 480):
     (H, W) = (480, 852)
-elif (args["res"] == 720):
+elif (int(args["res"]) == 720):
     (H, W) = (720, 1280)
-elif (args["res"] == 1080):
+elif (int(args["res"]) == 1080):
     (H, W) = (1080, 1920)
 else:
     (H, W) = (480, 852)
 
+cam = cv2.VideoCapture(int(args["input"]))
+
+s = time.time()
+for i in range(100):
+    ret, frame = cam.read()
+e = time.time()
+fps = 100/(e-s)
+print("[INFO] Estimated Camera FPS: {:.4f}".format(fps))
+
+cam.release()
+
+counter = 0
+
 while(True):
+    
     start = time.time()
-    # cam = cv2.VideoCapture(args["input"])
-    cam = cv2.VideoCapture(0)
+
+    cam = cv2.VideoCapture(int(args["input"]))
+    # cam = cv2.VideoCapture(0)
     writer = None
-    outputName = "Capture"+str(args["res"])+"p-"+str(int(start))+".mp4"
+    outputName = "Capture"+str(args["res"])+"p-"+str(time.localtime(start).tm_year)+'_'+str(time.localtime(start).tm_mon)+'_'+str(time.localtime(start).tm_mday)+'_'+str(time.localtime(start).tm_hour)+'_'+str(time.localtime(start).tm_min)+".mp4"
 
     if(cam.isOpened() == False):
         print("[ERROR] Camera connection error")
     else:
-        while(time.time() <= (start+300)):
+        while(time.time() <= (start+950)):
             ret, frame = cam.read()
             
             if(not ret):
@@ -37,9 +52,8 @@ while(True):
             frame = cv2.resize(frame, (W, H))
 
             if writer is None:
-                fourcc = 0x00000021
-                writer = cv2.VideoWriter(outputName, fourcc, 30,
-                    (frame.shape[1], frame.shape[0]), True)
+                fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+                writer = cv2.VideoWriter(outputName, fourcc, int(fps), (frame.shape[1], frame.shape[0]), True)
                 print("[INFO] Recording: "+outputName)
                 
             writer.write(frame)
@@ -47,4 +61,11 @@ while(True):
         print("[INFO] Recording done, cleaning up...")
         writer.release()
         cam.release()
+    
+    counter+=1
 
+    if(counter > 5):
+        time.sleep(3600)
+        counter = 0
+    else:
+        time.sleep(600)
